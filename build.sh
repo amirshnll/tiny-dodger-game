f00aea207e3add3dbebef9084fae7c46a5f3d9e7
Amir Shokri <amirsh.nll@gmail.com>
2026-09-23 17:07:23 +0330

    update builder

--- build.sh
+++ build.sh
@@ -16,6 +16,10 @@ find "$project_dir" -mindepth 1 -maxdepth 1 \
   ! -name outputs ! -name build.sh ! -name README.md \
   -exec cp -R {} "$stage_dir/" \;
 
+find "$stage_dir" -type f \
+  \( -name .gitignore -o -name .gitkeep -o -name .DS_Store \) \
+  -delete
+
 for browser in chrome firefox; do
   archive="$output_dir/${plugin_name}-${browser}.zip"
   rm -f "$archive"
